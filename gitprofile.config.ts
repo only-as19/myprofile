6b2c654f284f435dc73b700f2e323e5a0b86b9e8
Asad <hellohello51981@gmail.com>
2025-08-08 22:21:28 +0500

    My profile update teh config file

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -229,7 +229,7 @@ const CONFIG = {
 
   // Optional Footer. Supports plain text or HTML.
   footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
+      class="text-primary" href="https://github.com/only-as19"
       target="_blank"
       rel="noreferrer"
     >GitProfile</a> and ❤️`,
